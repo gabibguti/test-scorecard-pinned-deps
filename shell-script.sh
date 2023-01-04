9a572098b1e7989242b320cf48c5ef4f854859af
Gabriela Gutierrez <gabigutierrez@google.com>
2023-01-04 13:03:24 -0300

    Add wget command to shell script and remove pip command

--- shell-script.sh
+++ shell-script.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # Not pinned shell script
 
-# Not pinned pip command
+# # Not pinned pip command
+# apt-get update
+# apt-get install python3.8
+# python -m pip install cibuildwheel
+
+# Not pinned download then run package
+# Install and run anaconda miniconda3 with wget
 apt-get update
-apt-get install python3.8
-python -m pip install cibuildwheel
+apt-get install wget
+wget -nv -nc -O Miniconda3.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+bash Miniconda3.sh -b -p /opt/python
